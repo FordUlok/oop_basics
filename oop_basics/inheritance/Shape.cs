2f65dcafdb7a625b1eded4047fc5af6a380b2997
FordUlok <beermiksa25@gmail.com>
2026-04-08 16:54:00 +0200

    Started ineritance practice

--- oop_basics/inheritance/Shape.cs
+++ oop_basics/inheritance/Shape.cs
@@ -4,6 +4,40 @@ abstract class Shape
 {
     private bool isHoley;
     private string color;
-    
-    
+
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+    public Shape(string color, bool isHoley)
+    {
+         this.color = color;
+         this.isHoley = isHoley;
+    }
+    public virtual bool Displayable
+    {
+        get
+        {
+            return false;
+        }
+    }
+    public void MakeHoley()
+    {
+        this.isHoley = true;
+    }
+
+    public abstract double Perimeter();
+    public abstract double Area();
+
+    public virtual void Copy(Shape other)
+    {
+        if (other != null)
+        {
+            this.color = other.color;
+            this.isHoley = other.isHoley;
+        }
+    }
+
+
 }
